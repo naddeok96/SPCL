8eb8f5a5528bd1555a638f9e42cf1cc74cb76c86
Kyle Naddeo <naddeok96@github.com>
2025-09-21 19:25:08 -0400

    modified subset

--- combine_files.py
+++ combine_files.py
@@ -45,23 +45,24 @@ if __name__ == "__main__":
     # Set the list of file paths (update as needed).
     file_paths = [
         "utils.py",
-        # "replay_buffer.py",
-        # "rl_agent.py",
+        "replay_buffer.py",
+        "rl_agent.py",
         "curriculum.py",
         "curriculum_env.py",
         # "generate_dataset.py",
-        # "off_policy_train.py",
+        "off_policy_train.py",
+        "config_off_policy_seq.yaml",
         # "on_policy_train.py",
         # "config.yaml",
 
-        # "analyze_history.py",
+        "analyze_history.py",
         "eval_population.py",
         "init_population.py",
         "evolve_population.py",
-        "merge_history.py",
+        # "merge_history.py",
         "population_utils.py",
-        # "run_evolution.sh"
-        "run_evolution_parallel.sh",
+        "run_evolution.sh"
+        # "run_evolution_parallel.sh",
         # "experiment_for_vec_speed.py"
     ]
     # Set the output file path.
